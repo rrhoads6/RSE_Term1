f038b2ed7ef48c256faf3b7c1bab2c6a42ef393b
rrhoads6 <rrhoads09@gmail.com>
2018-05-20 21:41:22 -0600

    Try.

---
+++ robot_software_engineer_term_1/project_2_robotic_arm_pick_and_place/project2_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/dell/udacity_projects/robot_software_engineer_term_1/project_2_robotic_arm_pick_and_place/project2_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/dell/udacity_projects/robot_software_engineer_term_1/ros_ws/test_ws/devel;/opt/ros/kinetic".split(';') if "/home/dell/udacity_projects/robot_software_engineer_term_1/ros_ws/test_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ robot_software_engineer_term_1/project_2_robotic_arm_pick_and_place/project2_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/dell/udacity_projects/robot_software_engineer_term_1/project_2_robotic_arm_pick_and_place/project2_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
