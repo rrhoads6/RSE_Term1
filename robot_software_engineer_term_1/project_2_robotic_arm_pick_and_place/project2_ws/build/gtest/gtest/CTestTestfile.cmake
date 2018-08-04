057582d202caf5eed155813155f8ec23aed4e72c
rrhoads6 <rrhoads09@gmail.com>
2018-08-04 10:06:50 -0600

    Removed devel and build

--- robot_software_engineer_term_1/project_2_robotic_arm_pick_and_place/project2_ws/build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/dell/udacity_projects/robot_software_engineer_term_1/project_2_robotic_arm_pick_and_place/project2_ws/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
